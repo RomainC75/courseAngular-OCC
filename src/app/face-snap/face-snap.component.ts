c55282e83b4740a9ec5ec4ccf7c0e096cab0a2bf
RomainC75 <rom.chenard@gmail.com>
2023-05-06 20:19:48 +0200

    2-4-personnalised property

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  alreadySnapped!: boolean
 
+  ngOnInit(): void {
+    this.title = "Archibald"
+    this.description = "Super"
+    this.createdDate = new Date()
+    this.snaps = 6
+    this.imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HjaXYfBJyxxdFySFZfbZ373t4Yw9mIcuPA&usqp=CAU"
+    this.alreadySnapped = false
+  }
+
+  onAddSnap(){
+    if(!this.alreadySnapped){
+      this.snaps++
+      this.alreadySnapped=true
+    }else{
+      this.snaps--
+      this.alreadySnapped=false
+    }
+  }
 }
